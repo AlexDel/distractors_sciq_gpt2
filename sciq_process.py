0680646f4d2760a5316923cfe6e8e7549fb7884d
Alexandr Kalinin <akalinin@issart.com>
2020-03-13 18:25:10 +0700

    Initial commit

---
+++ sciq_process.py
@@ -0,0 +1,34 @@
+import json
+import os
+import random
+
+from special_tokens import tokens_set
+
+
+def convert_to_text(json_path_dir: str) -> None:
+    samples = []
+    with open(os.path.join(os.getcwd(), json_path_dir, 'train.json')) as content:
+        json_sciq = json.loads(content.read())
+ 
+    for item in json_sciq:
+        for i in range(1, 4):
+            sample_string = ' '.join([
+                tokens_set['support_token'],
+                item['support'],
+                tokens_set['question_token'],
+                item['question'],
+                tokens_set['key_token'],
+                item['correct_answer'],
+                tokens_set['distractor_token'],
+                item[f'distractor{i}'],
+                '<|endoftext|>',
+            ])
+
+            samples.append(sample_string.strip())
+
+    random.shuffle(samples)
+
+    with open(os.path.join(os.getcwd(), json_path_dir, 'converted.txt'), 'w') as f:
+        f.write('\n'.join(samples))
+
+convert_to_text('sciq')
---
+++ special_tokens.py
@@ -0,0 +1,6 @@
+tokens_set = dict(
+    question_token = '<|question|>',
+    support_token = '<|support|>',
+    key_token = '<|key|>',
+    distractor_token = '<|distractor|>'
+)
