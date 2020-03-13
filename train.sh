37f77529f560f9f599aceba92dd907fb3336c153
Alexandr Kalinin <akalinin@issart.com>
2020-03-13 18:54:53 +0700

    train.sh.

---
+++ train.sh
@@ -0,0 +1,8 @@
+python f_tune.py
+    --output_dir=output
+    --model_type=gpt2
+    --model_name_or_path=gpt2
+    --do_train
+    --train_data_file=./sciq/train.txt
+    --do_eval
+    --eval_data_file=./sciq/test.txt
