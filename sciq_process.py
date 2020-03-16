5a4e5186bd49956c02d630f34a899e84abb8e668
Alexandr Kalinin <akalinin@issart.com>
2020-03-16 14:44:53 +0700

    Test and train set remade.

--- sciq_process.py
+++ sciq_process.py
@@ -7,28 +7,29 @@ from special_tokens import tokens_set
 
 def convert_to_text(json_path_dir: str) -> None:
     samples = []
-    with open(os.path.join(os.getcwd(), json_path_dir, 'test.json')) as content:
-        json_sciq = json.loads(content.read())
- 
-    for item in json_sciq:
-        for i in range(1, 4):
-            sample_string = ' '.join([
-                tokens_set['support_token'],
-                item['support'],
-                tokens_set['question_token'],
-                item['question'],
-                tokens_set['key_token'],
-                item['correct_answer'],
-                tokens_set['distractor_token'],
-                item[f'distractor{i}'],
-                '<|endoftext|>',
-            ])
+    for d_set in ['train', 'test']:
+        with open(os.path.join(os.getcwd(), json_path_dir, f'{d_set}.json')) as content:
+            json_sciq = json.loads(content.read())
 
-            samples.append(sample_string.strip())
+        for item in json_sciq:
+            for i in range(1, 4):
+                sample_string = ' '.join([
+                    # tokens_set['support_token'],
+                    # item['support'],
+                    tokens_set['question_token'],
+                    item['question'],
+                    tokens_set['key_token'],
+                    item['correct_answer'],
+                    tokens_set['distractor_token'],
+                    item[f'distractor{i}'],
+                    '<|endoftext|>',
+                ])
 
-    random.shuffle(samples)
+                samples.append(sample_string.strip())
 
-    with open(os.path.join(os.getcwd(), json_path_dir, 'test.txt'), 'w') as f:
-        f.write('\n'.join(samples))
+        random.shuffle(samples)
+
+        with open(os.path.join(os.getcwd(), json_path_dir, f'{d_set}.txt'), 'w') as f:
+            f.write('\n'.join(samples))
 
 convert_to_text('sciq')
