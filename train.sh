25588d959368468f26eab87dc6216faa5b3e9ef5
Alexandr Kalinin <akalinin@issart.com>
2020-03-20 12:38:39 +0700

    Revert "Model changed."
    This reverts commit 2b38310f

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python f_tune.py \
   --output_dir=output \
-  --model_type=gpt2-xl \
-  --model_name_or_path=gpt2-xl \
+  --model_type=gpt2 \
+  --model_name_or_path=gpt2 \
   --do_train \
   --train_data_file=./sciq/train.txt \
   --do_eval \
